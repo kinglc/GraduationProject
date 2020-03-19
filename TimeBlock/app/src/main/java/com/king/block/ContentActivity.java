02bbc95c358d2356df08cc3d9f973e02e1a63c3d
kingc <1126663073@qq.com>
2020-03-19 14:00:07 +0800

    日期完成

--- TimeBlock/app/src/main/java/com/king/block/ContentActivity.java
+++ TimeBlock/app/src/main/java/com/king/block/ContentActivity.java
@@ -2,18 +2,27 @@ package com.king.block;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class ContentActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private LinearLayout note,list,plan,user;
-    private ImageView note_pic,list_pic,plan_pic,user_pic;
-    private TextView note_txt,list_txt,plan_txt,user_txt;
+    private LinearLayout note,list,plan;
+    private ImageView note_pic,list_pic,plan_pic;
+    private TextView note_txt,list_txt,plan_txt;
+
+    private TextView date;
+    private int year,month,day;
+
+    private int index;//0-今日待办，1-计划系统，2-备忘录
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,37 +32,59 @@ public class ContentActivity extends AppCompatActivity implements View.OnClickLi
 //        String name = intent.getStringExtra("name");//////////////////////////////////////////////
 //        int age = intent.getIntExtra("age");
 
-        initNavi();
+        initComp();
+        initEvent();
         initStyle();
-        initFrag(2);
+        initFrag();
+
+        //初始计划页面
+        index=1;
+        plan_pic.setImageResource(R.drawable.plan_selected);
+        plan_txt.setTextColor(Color.parseColor("#3FC1EB"));
 
 
     };
 
-    //初始化导航栏组件
-    private void initNavi(){
+    //初始化组件
+    private void initComp(){
         note = (LinearLayout)findViewById(R.id.note);
         list = (LinearLayout)findViewById(R.id.list);
         plan = (LinearLayout)findViewById(R.id.plan);
-        user = (LinearLayout)findViewById(R.id.user);
 
         note_pic = (ImageView)findViewById(R.id.note_pic);
         list_pic = (ImageView)findViewById(R.id.list_pic);
         plan_pic = (ImageView)findViewById(R.id.plan_pic);
-        user_pic = (ImageView)findViewById(R.id.user_pic);
 
         note_txt=(TextView)findViewById(R.id.note_txt);
         list_txt=(TextView)findViewById(R.id.list_txt);
         plan_txt=(TextView)findViewById(R.id.plan_txt);
-        user_txt=(TextView)findViewById(R.id.user_txt);
 
+        date = (TextView) findViewById(R.id.date);
+    }
+
+    //初始化事件
+    private void initEvent(){
         note.setOnClickListener(this);
         list.setOnClickListener(this);
         plan.setOnClickListener(this);
-        user.setOnClickListener(this);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ContentActivity.this, new DatePickerDialog.OnDateSetListener() {      //  日期选择对话框
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        //  这个方法是得到选择后的 年，月，日，分别对应着三个参数 — year、month、dayOfMonth
+                        String s = year + "-" + month;
+                        if(index==1) s+="-" + dayOfMonth;
+                        date.setText(s+ "  ▼");
+                    }
+                }, year, month, day).show();   //  弹出日历对话框时，默认显示 年，月，日
+            }
+        });
     }
 
-    //初始化导航栏样式
+    //重置初始化样式
     private void initStyle(){
         note_pic.setImageResource(R.drawable.note);
         note_txt.setTextColor(Color.parseColor("#bfbfbf"));
@@ -61,45 +92,49 @@ public class ContentActivity extends AppCompatActivity implements View.OnClickLi
         list_txt.setTextColor(Color.parseColor("#bfbfbf"));
         plan_pic.setImageResource(R.drawable.plan);
         plan_txt.setTextColor(Color.parseColor("#bfbfbf"));
-        user_pic.setImageResource(R.drawable.user);
-        user_txt.setTextColor(Color.parseColor("#bfbfbf"));
-    }
 
-    //初始化fragment
-    private void initFrag(int index){
-
+        Calendar mcalendar = Calendar.getInstance();     //  获取当前时间    —   年、月、日
+        year = mcalendar.get(Calendar.YEAR);         //  得到当前年
+        month = mcalendar.get(Calendar.MONTH);       //  得到当前月
+        day = mcalendar.get(Calendar.DAY_OF_MONTH);  //  得到当前日
+        String s = year + "-" + month;
+        if(index==1) s+="-" + day;
+        date.setText(s+ "  ▼");
     }
 
     //修改导航栏样式，切换fragment
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.note:
-                initStyle();
-                note_pic.setImageResource(R.drawable.note_selected);
-                note_txt.setTextColor(Color.parseColor("#3FC1EB"));
-                initFrag(0);
-                break;
             case R.id.list:
+                index=0;
                 initStyle();
                 list_pic.setImageResource(R.drawable.list_selected);
                 list_txt.setTextColor(Color.parseColor("#3FC1EB"));
-                initFrag(1);
+                initFrag();
                 break;
             case R.id.plan:
+                index=1;
                 initStyle();
                 plan_pic.setImageResource(R.drawable.plan_selected);
                 plan_txt.setTextColor(Color.parseColor("#3FC1EB"));
-                initFrag(2);
+                initFrag();
                 break;
-            case R.id.user:
+            case R.id.note:
+                index=2;
                 initStyle();
-                user_pic.setImageResource(R.drawable.user_selected);
-                user_txt.setTextColor(Color.parseColor("#3FC1EB"));
-                initFrag(3);
+                note_pic.setImageResource(R.drawable.note_selected);
+                note_txt.setTextColor(Color.parseColor("#3FC1EB"));
+                initFrag();
                 break;
             default:break;
         }
     }
+
+    //初始化fragment
+    private void initFrag(){
+
+    }
+
 
 }
