5e4f9d36eccdae672b3c2e07e254a2e519e9207c
kingc <1126663073@qq.com>
2020-04-22 22:48:42 +0800

    计时器调试前

--- TimeBlock/app/src/main/java/com/king/block/content/PlanFragment.java
+++ TimeBlock/app/src/main/java/com/king/block/content/PlanFragment.java
@@ -41,10 +41,9 @@ public class PlanFragment extends Fragment{
     TextView plan_title;
     TextView plan_content;
     TextView plan_ddl;
-    TextView plan_pass;
     ImageView select;
-    int on;//0-暂停 1-计时
-    private Chronometer timer;
+    private Chronometer pass;
+    int on = 0;//1-暂停 0-计时
     private long baseTimer;
 
     @Override
@@ -56,7 +55,6 @@ public class PlanFragment extends Fragment{
         initEvent();
         setNow();
 
-
         return view;
     }
 
@@ -64,7 +62,7 @@ public class PlanFragment extends Fragment{
         //未完成 获取数据
         for(int i=0;i<10;i++) {
             Plan plan = new Plan(i,i+""+i+i+i+i, "间关莺语花底滑，幽咽泉流冰下难",
-                    i%4,"00:0"+i,"-1","2011-01-02","01:01");
+                    i%4,"00:00:0"+i,"-1","2011-01-02","01:01");
             plan_list.add(plan);
         }
     }
@@ -80,76 +78,74 @@ public class PlanFragment extends Fragment{
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                plan_now = getNow();
                 plan_now = plan_list.get(position);
-                setNow();;
+                setNow();
             }
         });
-    }
-
-    public void startTimer() {
-        timer.setBase(SystemClock.elapsedRealtime());//计时器清零
-        int hour = (int) ((SystemClock.elapsedRealtime() - timer.getBase()) / 1000 / 60);
-        timer.setFormat("0"+String.valueOf(hour)+":%s");
-        timer.setBase(SystemClock.elapsedRealtime()-300*1000);//从5分钟开始计时
-        timer.start();//开启计时
     }
 
     private void initEvent() {
         plan_title = (TextView)view.findViewById(R.id.plan_title);
         plan_content = (TextView)view.findViewById(R.id.plan_content);
         plan_ddl = (TextView)view.findViewById(R.id.plan_ddl);
-        plan_pass = (TextView)view.findViewById(R.id.plan_pass);
+        pass = (Chronometer) view.findViewById(R.id.plan_pass);
+        pass.setFormat("00:00:00");
         select = (ImageView)view.findViewById(R.id.select);
         select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeOn();
+//                changeOn(1);
+                pass.setBase(5);
+                pass.start();
             }
         });
 
-
     }
 
-    private void changeOn(){
-        if(plan_now==null) return;
-        if(on==0){
-            select.setImageResource(R.drawable.pause);
-            on = 1;
-        }else{
-            select.setImageResource(R.drawable.on);
-            on = 1;
-        }
+
+    private void changeOn(long passtime){
+//        if(plan_now==null) return;
+//        if(on==1){
+//            select.setImageResource(R.drawable.pause);
+//            pass.setBase(passtime);
+//            pass.start();
+//        }else{
+//            select.setImageResource(R.drawable.on);
+//            pass.stop();
+//        }
+//        on=1-on;
     }
 
     private void setNow(){
-        //未完成-获取on状态
-        on=1;
-        changeOn();
+//        if(plan_now!=null){
+//            plan_title.setText(plan_now.getTitle());
+//            plan_content.setText(plan_now.getContent());
+//            plan_ddl.setText(plan_now.getDate()+" "+plan_now.getTime());
+////            plan_pass.setText(plan_now.getPass());
+//            on=0;
+//            //未完成-获取on状态
+//            long passtime=5;
+//            changeOn(passtime);
+//            switch (plan_now.getUrgency()){
+//                case 0:
+//                    pass.setTextColor(getContext().getResources().getColor(R.color.red));
+//                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.red));
+//                    break;
+//                case 1:
+//                    pass.setTextColor(getContext().getResources().getColor(R.color.yellow));
+//                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.yellow));
+//                    break;
+//                case 2:
+//                    pass.setTextColor(getContext().getResources().getColor(R.color.green));
+//                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.green));
+//                    break;
+//                case 3:
+//                    pass.setTextColor(getContext().getResources().getColor(R.color.blue));
+//                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.blue));
+//                    break;
+//                default:break;
+//            }
+//        }
 
-        if(plan_now!=null){
-            plan_title.setText(plan_now.getTitle());
-            plan_content.setText(plan_now.getContent());
-            plan_ddl.setText(plan_now.getDate()+" "+plan_now.getTime());
-            plan_pass.setText(plan_now.getPass());
-            switch (plan_now.getUrgency()){
-                case 0:
-                    plan_pass.setTextColor(getContext().getResources().getColor(R.color.red));
-                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.red));
-                    break;
-                case 1:
-                    plan_pass.setTextColor(getContext().getResources().getColor(R.color.yellow));
-                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.yellow));
-                    break;
-                case 2:
-                    plan_pass.setTextColor(getContext().getResources().getColor(R.color.green));
-                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.green));
-                    break;
-                case 3:
-                    plan_pass.setTextColor(getContext().getResources().getColor(R.color.blue));
-                    plan_ddl.setTextColor(getContext().getResources().getColor(R.color.blue));
-                    break;
-                default:break;
-            }
-        }
     }
 
     public void Load() {
