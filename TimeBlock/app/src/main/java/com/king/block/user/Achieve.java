6ed73c23caaa9040adf666cccab3dbbbd9164418
kingc <1126663073@qq.com>
2020-04-30 00:30:36 +0800

    achieve前期

---
+++ TimeBlock/app/src/main/java/com/king/block/user/Achieve.java
@@ -0,0 +1,37 @@
+package com.king.block.user;
+
+public class Achieve {
+    int id;
+    int type;//0-金，1-银，2-铜，-1 -无
+    String name;
+
+    public Achieve(int id, int type, String name) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
