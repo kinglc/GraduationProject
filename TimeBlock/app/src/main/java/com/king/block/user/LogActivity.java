51e4f7aa95ed2aa784f1a5a8d2ab81d8ed1b0e0b
kingc <1126663073@qq.com>
2020-05-03 17:09:25 +0800

    完成log

--- TimeBlock/app/src/main/java/com/king/block/user/LogActivity.java
+++ TimeBlock/app/src/main/java/com/king/block/user/LogActivity.java
@@ -5,25 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.king.block.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LogActivity extends AppCompatActivity {
+    View view;
+    private ListView log_lv;
+    private LogAdapter logAdapter;
+    public static List<Log> log_list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
         initTop();
+        initData();
+        initLv();
     }
 
-    private void initTop(){
-        ImageView back = (ImageView)findViewById(R.id.back);
+    private void initTop() {
+        ImageView back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LogActivity.this.finish();
             }
         });
+    }
+
+    private void initData() {
+        //未完成 获取数据
+        for (int i = 0; i < 10; i++) {
+            Log log = new Log(i, i % 3, i);
+            log_list.add(log);
+        }
+    }
+
+    private void initLv() {
+        logAdapter = new LogAdapter(LogActivity.this, R.layout.item_log, log_list);
+        log_lv = (ListView) findViewById(R.id.log_lv);
+        log_lv.setAdapter(logAdapter);
     }
 }
--- TimeBlock/app/src/main/java/com/king/block/user/LogAdapter.java
+++ TimeBlock/app/src/main/java/com/king/block/user/LogAdapter.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.king.block.R;
+import com.king.block.content.Plan;
+import com.king.block.content.Todo;
 
 import java.util.List;
 
@@ -35,14 +37,28 @@ public class LogAdapter extends ArrayAdapter<Log> {
         }else{
             viewHolder = (LogAdapter.ViewHolder) convertView.getTag();
         }
-        viewHolder.rank.setText(position+"");
-        viewHolder.name.setText(log.getType());
-        viewHolder.time.setText(log.getLogId());
-        switch (log.getType()){
-            case 0:viewHolder.type.setImageResource(R.drawable.copper_plan);break;
-            case 1:viewHolder.type.setImageResource(R.drawable.copper_plan);break;
-            case 2:viewHolder.type.setImageResource(R.drawable.copper_plan);break;
-            default:;break;
+        //未完成-获取对应事项内容及事件
+        int id = log.getLogId();
+        switch (log.getType()) {
+            case 0:
+                viewHolder.type.setImageResource(R.drawable.log_todo);
+                Todo todo = new Todo(1,"aaa",true,"2020-01-01","2020-01-01 01:01");
+                viewHolder.time.setText(todo.getTime());
+                viewHolder.content.setText("完成当日待办");
+                break;
+            case 1:
+                viewHolder.type.setImageResource(R.drawable.log_plan);
+                Plan plan = new Plan(1,"计划B","111",1,",,","2020-03-03 03:03","111","111");
+                viewHolder.time.setText(plan.getFinish());
+                viewHolder.content.setText("完成计划"+plan.getTitle());
+                break;
+            case 2:
+                viewHolder.type.setImageResource(R.drawable.log_achieve);
+                Achieve achieve = new Achieve(1,1,"成就C","2020-03-03 03:03");
+                viewHolder.time.setText(achieve.getTime());
+                viewHolder.content.setText("获得成就"+achieve.getName());
+                break;
+            default:break;
         }
         return convertView;
     }
