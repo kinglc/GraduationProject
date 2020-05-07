e762f1839b60cd1182fab26333278af5f24c1684
kingc <1126663073@qq.com>
2020-05-08 00:55:38 +0800

    colchart成形

--- TimeBlock/app/src/main/java/com/king/block/user/ChartActivity.java
+++ TimeBlock/app/src/main/java/com/king/block/user/ChartActivity.java
@@ -2,11 +2,9 @@ package com.king.block.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,16 +13,19 @@ import android.widget.Toast;
 
 import com.king.block.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.ColumnChartOnValueSelectListener;
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Column;
 import lecho.lib.hellocharts.model.ColumnChartData;
-import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.util.ChartUtils;
 import lecho.lib.hellocharts.view.ColumnChartView;
 import lecho.lib.hellocharts.view.PieChartView;
@@ -47,7 +48,6 @@ public class ChartActivity extends AppCompatActivity {
             {"1","2","3","4","5","6","7","8","9","10","11","12"}};
     PieChartView pie_chart;
     ColumnChartView col_chart;
-    private List<PointValue> mPointValues = new ArrayList<PointValue>();
     private List<AxisValue> mAxisXValues = new ArrayList<AxisValue>();
 
     @Override
@@ -106,7 +106,7 @@ public class ChartActivity extends AppCompatActivity {
         wDay = c.get(Calendar.DAY_OF_MONTH);
         getEndDay();
         date.setText(wYear + "年" + wMonth + "月" + wDay + "日 ~ " + wYearEnd + "年" +wMonthEnd + "月" +  wDayEnd + "日");
-        initChart();
+        initColChart();
     }
 
     private void getEndDay() {
@@ -195,7 +195,7 @@ public class ChartActivity extends AppCompatActivity {
             yYear+=1*minu;
             date.setText(yYear + "年" );
         }
-        initChart();
+        initColChart();
     }
 
     private int getMonthDay(){
@@ -208,106 +208,69 @@ public class ChartActivity extends AppCompatActivity {
         return maxDate;
     }
 
-    private void initChart(){
-        Toast.makeText(ChartActivity.this,"aaa",Toast.LENGTH_SHORT).show();
+    private void initColChart(){
 
-        ColumnChartView chart;            //柱状图的自定义View
         ColumnChartData data;             //存放柱状图数据的对象
-        boolean hasAxes = true;            //是否有坐标轴
-        boolean hasAxesNames = true;       //是否有坐标轴的名字
-        boolean hasLabels = false;          //柱子上是否显示标识文字
-        boolean hasLabelForSelected = true;    //柱子被点击时，是否显示标识的文字
-
-        int numSubcolumns = 1;
-        int numColumns = 8;
-        // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
+        int numColumns;
+        List<AxisValue> axisValues = new ArrayList<AxisValue>();
         List<Column> columns = new ArrayList<Column>();
         List<SubcolumnValue> values;
+        int hour[]=new int[32];
+        type=1;
+
+        //初始化x轴
+        if(type==1){
+            numColumns=getMonthDay();
+        } else {
+            numColumns = x[type].length;
+        }
+        for (int i = 0; i < numColumns; i++) {
+            mAxisXValues.add(new AxisValue(i).setLabel(x[type][i]));
+            hour[i]=i%24;//待删
+        }
+
+        //未完成-获取y数据
+        //hour
+
+        //设置每个柱
         for (int i = 0; i < numColumns; ++i) {
-
             values = new ArrayList<SubcolumnValue>();
-            for (int j = 0; j < numSubcolumns; ++j) {
-                values.add(new SubcolumnValue((float) Math.random() * 50f + 5, ChartUtils.pickColor()));
-            }
-
-            Column column = new Column(values);
-//            column.setHasLabels(hasLabels);
-//            column.setHasLabelsOnlyForSelected(hasLabelForSelected);
-            columns.add(column);
+            values.add(new SubcolumnValue(hour[i], ChartUtils.pickColor()));
+            columns.add(new Column(values).setHasLabelsOnlyForSelected(true));
         }
 
         data = new ColumnChartData(columns);
 
-        Axis axisX = new Axis();
-        Axis axisY = new Axis().setHasLines(true);
-        axisX.setName("Axis X");
-        axisY.setName("Axis Y");
+        data.setAxisXBottom(new Axis(axisValues).setHasLines(true)
+                .setTextColor(Color.BLACK));
+        data.setAxisYLeft(new Axis().setHasLines(true)
+                .setTextColor(Color.BLACK).setMaxLabelChars(2));
+
+        col_chart.setColumnChartData(data);
+
+// Set value touch listener that will trigger changes for chartTop.
+        col_chart.setOnValueTouchListener(new ValueTouchListener(){
+        });
+
+// Set selection mode to keep selected month column highlighted.
+        col_chart.setValueSelectionEnabled(true);
+        col_chart.setInteractive(true);
+        col_chart.setZoomType(ZoomType.HORIZONTAL);
 
     }
 
-//    private void initX(){
-//        for (int i = 0; i < x[type].length; i++) {
-//            if(type==1&&i==getMonthDay()) break;
-//            mAxisXValues.add(new AxisValue(i).setLabel(x[type][i]));
-//        }
-//    }
-//
-//    private void initPoint(){
-//        //未完成-获取对应时间
-//        for (int i = 0; i < 7; i++) {
-//            mPointValues.add(new PointValue(i, i));
-//        }
-//    }
+    private class ValueTouchListener implements ColumnChartOnValueSelectListener {
 
-//    private void initChart(){
-//        Column col = new Column(ChartActivity.this);
-////                Column(mPointValues).setColor(Color.parseColor("#FFCD41"));  //折线的颜色（橙色）
-//        List<Column> cols = new ArrayList<Column>();
-//        col.set
-//        col.setShape(ValueShape.CIRCLE);//折线图上每个数据点的形状  这里是圆形 （有三种 ：ValueShape.SQUARE  ValueShape.CIRCLE  ValueShape.DIAMOND）
-//        col.setCubic(false);//曲线是否平滑，即是曲线还是折线
-//        col.setFilled(false);//是否填充曲线的面积
-//        col.setHasLabels(true);//曲线的数据坐标是否加上备注
-////      line.setHasLabelsOnlyForSelected(true);//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
-//        col.setHasLines(true);//是否用线显示。如果为false 则没有曲线只有点显示
-//        col.setHasPoints(true);//是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
-//        cols.add(line);
-//        ColumnChartData data = new ColumnChartData();
-//        data.setColumns(cols);
-//
-//        //坐标轴
-//        Axis axisX = new Axis(); //X轴
-//        axisX.setHasTiltedLabels(true);  //X坐标轴字体是斜的显示还是直的，true是斜的显示
-//        axisX.setTextColor(Color.GRAY);  //设置字体颜色
-//        //axisX.setName("date");  //表格名称
-//        axisX.setTextSize(10);//设置字体大小
-//        axisX.setMaxLabelChars(20); //最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
-//        axisX.setValues(mAxisXValues);  //填充X轴的坐标名称
-//        data.setAxisXBottom(axisX); //x 轴在底部
-//        //data.setAxisXTop(axisX);  //x 轴在顶部
-//        axisX.setHasLines(true); //x 轴分割线
-//
-//        // Y轴是根据数据的大小自动设置Y轴上限(在下面我会给出固定Y轴数据个数的解决方案)
-//        Axis axisY = new Axis();  //Y轴
-//        axisY.setName("");//y轴标注
-//        axisY.setTextSize(10);//设置字体大小
-//        data.setAxisYLeft(axisY);  //Y轴设置在左边
-//        //data.setAxisYRight(axisY);  //y轴设置在右边
-//
-//
-//        //设置行为属性，支持缩放、滑动以及平移
-//        lineChart.setInteractive(true);
-//        lineChart.setZoomType(ZoomType.HORIZONTAL);
-//        lineChart.setMaxZoom((float) 2);//最大方法比例
-//        lineChart.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
-//        lineChart.setLineChartData(data);
-//        lineChart.setVisibility(View.VISIBLE);
-//        /**注：下面的7，10只是代表一个数字去类比而已
-//         * 当时是为了解决X轴固定数据个数。见（http://forum.xda-developers.com/tools/programming/library-hellocharts-charting-library-t2904456/page2）;
-//         */
-//        Viewport v = new Viewport(lineChart.getMaximumViewport());
-//        v.left = 0;
-//        v.right = 7;
-//        lineChart.setCurrentViewport(v);
-//    }
+        @Override
+        public void onValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+//            initPieChart(columnIndex);
+        }
+
+        @Override
+        public void onValueDeselected() {
+
+        }
+
+    }
+
 }
