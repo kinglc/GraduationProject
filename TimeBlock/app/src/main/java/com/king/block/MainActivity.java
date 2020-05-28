63334647933eea559719936b434226f450a4c3bb
kingc <1126663073@qq.com>
2020-05-28 17:43:01 +0800

    更新ip

--- TimeBlock/app/src/main/java/com/king/block/MainActivity.java
+++ TimeBlock/app/src/main/java/com/king/block/MainActivity.java
@@ -54,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         global = (Global)getApplication();
-        global.setURL("http://10.0.2.2:8080");
-//        global.setURL("http://140.143.78.135:8080");
+//        global.setURL("http://10.0.2.2:8080");
+        global.setURL("http://140.143.78.135:8080");
 
         regi = (Button) findViewById(R.id.regi);
         input = (EditText)findViewById(R.id.input);
