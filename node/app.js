7548baade9407d99c58aeee9dff6f3f6bfa2c491
kingc <1126663073@qq.com>
2020-06-08 02:12:37 +0800

    markk

--- node/app.js
+++ node/app.js
@@ -13,8 +13,7 @@ app.all("*", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "*");
-    res.header("X-Powered-By", "http://www.shuzhiqiang.com");
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Content-Type", "application/json;charset=utf-8");//数据形式为json
     res.header("Access-Control-Allow-Credentials",true);//携带cookie跨域请求
     req.method.toUpperCase() === "OPTIONS" ? res.sendStatus(200) : next();//防止在预请求阶段就响应接口
 });
